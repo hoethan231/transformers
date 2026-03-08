82dd35c91145dea4388fbde64c81a2ef3e455c70
hoethan231 <ethanho231@gmail.com>
2026-03-08 13:20:03 -0700

    implement multiheaded attention

--- self_attention.py
+++ self_attention.py
@@ -14,15 +14,38 @@ class SelfAttention(nn.Module):
 		self.W_k = nn.Linear(in_features=d, out_features=d, bias=False)
 		self.W_v = nn.Linear(in_features=d, out_features=d, bias=False)
 
-	def forward(self, embeddings):
-		q = self.W_q(embeddings)
-		k = self.W_k(embeddings)
-		v = self.W_v(embeddings)
+	def forward(self, q_embed, k_embed, v_embed, mask=None):
+		q = self.W_q(q_embed)
+		k = self.W_k(k_embed)
+		v = self.W_v(v_embed)
 
 		QK = torch.matmul(q, k.transpose(self.row, self.col))
 		scaled_QK = QK / torch.tensor(k.size(self.col)**0.5)
+
+		if mask not None:
+			scaled_QK = scaled_QK.masked_fill(mask=mask, value=-1e9)
+
 		attention_per = F.softmax(QK_dim, self-col)
 		attention_val = torch.matmul(attention_per, v)
 		
 		return attention_val
+
+
+class MultiHeaddedAttention(nn.Module):
+
+	def __init__(self, d=2, row=0, col=1, heads=1):
+		self.row = row
+		self.col = col
+
+		super().__init__()
+
+		self.heads = nn.ModuleList(
+			SelfAttention(d, row, col) for _ in range(heads)
+		)
+
+
+	def foward(self, q_embed, k_embed, v_embed):
+		return torch.cat(
+			[head(q_embed, k_embed, v_embed) for head in self.heads]
+				, dim=self.col)
 
