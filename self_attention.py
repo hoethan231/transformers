009b0161ca17d5136abba708ffc8a9255cd2c9f6
hoethan231 <ethanho231@gmail.com>
2026-03-08 12:08:38 -0700

    edit self attention

--- self_attention.py
+++ self_attention.py
@@ -5,19 +5,24 @@ import torch.nn.functional as F
 class SelfAttention(nn.Module):
 
 	def __init__(self, d, row=0, col=1):
-		self.d = d
 		self.row = row
 		self.col = col
+		
+		super().__init__()	
 
-		self.W_q = nn.Linear(d, d)
-		self.W_k = nn.Linear(d, d)
-		self.W_v = nn.Linear(d, d)
+		self.W_q = nn.Linear(in_features=d, out_features=d, bias=False)
+		self.W_k = nn.Linear(in_features=d, out_features=d, bias=False)
+		self.W_v = nn.Linear(in_features=d, out_features=d, bias=False)
 
 	def forward(self, embeddings):
-		QK = torch.matmul(q, torch.transpose(k))
-		QK_dim = QK / self.W_k.shape[1]
-		attention_per = F.softmax(QK_dim)
-		attention_val = toch.matmul(attention_per, torch.transpose(self.W_v))
+		q = self.W_q(embeddings)
+		k = self.W_k(embeddings)
+		v = self.W_v(embeddings)
+
+		QK = torch.matmul(q, k.transpose(self.row, self.col))
+		scaled_QK = QK / torch.tensor(k.size(self.col)**0.5)
+		attention_per = F.softmax(QK_dim, self-col)
+		attention_val = torch.matmul(attention_per, v)
 		
 		return attention_val
 
